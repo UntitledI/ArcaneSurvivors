f0585d2ff1678411c3bdf0ccc5d4ef4a95128a96
jonathanluu0 <jonathanluu0@gmail.com>
2023-04-09 20:43:30 -0700

    Animated Player

--- Assets/Scenes/team/JONATHAN/ThirdPersonMovement.cs
+++ Assets/Scenes/team/JONATHAN/ThirdPersonMovement.cs
@@ -6,6 +6,7 @@ public class ThirdPersonMovement : MonoBehaviour
 {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     //Essentials
     public CharacterController controller;
     public Transform cam;
@@ -75,21 +76,43 @@ public class ThirdPersonMovement : MonoBehaviour
 =======
 =======
 >>>>>>> a0fce318 (readded Character)
+=======
+    //Essentials
+>>>>>>> 2dd69085 (Animated Player)
     public CharacterController controller;
     public Transform cam;
-    public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     public float turnSmoothVelocity;
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> eff5772a (Player Movement repush)
     public Animator animator;
+=======
+    //Movement
+    public float walkSpeed;
+    public float sprintSpeed;
+    bool sprinting;
+    float trueSpeed;
+
+    //Jumping
+    public float jumpHeight;
+    public float gravity;
+    bool isGrounded;
+    Vector3 velocity;
+
+    private Animator animator;
+>>>>>>> 2dd69085 (Animated Player)
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+        trueSpeed = walkSpeed;
+        animator = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
 <<<<<<< HEAD
@@ -100,10 +123,33 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
+        animator.SetBool("IsGrounded", isGrounded);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1;
+        }
+
+        //Enables Sprinting
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            trueSpeed = sprintSpeed;
+            sprinting = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            trueSpeed = walkSpeed;
+            sprinting = false;
+        }
+        //retains animations positions and angles
+        animator.transform.localPosition = Vector3.zero;
+        animator.transform.localEulerAngles = Vector3.zero;
         //retrieves cursor coordinate input
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         //Moves the character in the direction of the camera
@@ -119,6 +165,9 @@ public class ThirdPersonMovement : MonoBehaviour
         if(direction.magnitude >= 0.1f)
 >>>>>>> a0fce318 (readded Character)
 =======
+=======
+
+>>>>>>> 2dd69085 (Animated Player)
         //Moves the character in the direction of the camera
         if (direction.magnitude >= 0.1f)
 >>>>>>> eff5772a (Player Movement repush)
@@ -135,6 +184,9 @@ public class ThirdPersonMovement : MonoBehaviour
             //Moves the character through characterController object
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 2dd69085 (Animated Player)
             controller.Move(moveDir.normalized * trueSpeed * Time.deltaTime);
 
             //flags to determine when player is walking/sprinting
@@ -146,6 +198,7 @@ public class ThirdPersonMovement : MonoBehaviour
             {
                 animator.SetFloat("Speed", 1);
             }
+<<<<<<< HEAD
         }
         else
         {
@@ -166,7 +219,24 @@ public class ThirdPersonMovement : MonoBehaviour
 
 =======
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+=======
+>>>>>>> 2dd69085 (Animated Player)
         }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
+        //Jumping
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
+        }
+
+        if(velocity.y > -20)
+        {
+            velocity.y += (gravity * 10) * Time.deltaTime;
+        }
+        controller.Move(velocity * Time.deltaTime);
     }
 >>>>>>> 4a281c55 (readded Character)
 =======

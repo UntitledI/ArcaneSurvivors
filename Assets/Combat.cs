4bc0c9c663ee26e736c4b1c732452b4507b024ca
jonathanluu0 <jonathanluu0@gmail.com>
2023-04-13 21:01:28 -0700

    Polished Player Movement/Camera

---
+++ Assets/Combat.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    private Animator anim;
+    public float cooldownTime = 2f;
+    private float nextFireTime = 0f;
+    public static int noOfClicks = 0;
+    float lastClickedTime = 0;
+    float maxComboDelay = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if(noOfClicks == 1)
+        {
+
+        }
+    }
+}

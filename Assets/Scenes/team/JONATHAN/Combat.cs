aa669ed20738a81a958543a71685b91358eb98d4
jonathanluu0 <jonathanluu0@gmail.com>
2023-04-19 22:08:46 -0700

    Fixed Combat Anims

--- Assets/Scenes/team/JONATHAN/Combat.cs
+++ Assets/Scenes/team/JONATHAN/Combat.cs
@@ -10,34 +10,40 @@ public class Combat : MonoBehaviour
     public static int noOfClicks = 0;
     float lastClickedTime = 0;
     float maxComboDelay = 1;
-    // Start is called before the first frame update
+
     void Start()
     {
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f)
         {
-            anim.SetBool("Hit1", false);
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+            {
+                anim.SetBool("Hit1", false);
+            }
+
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
+            {
+                anim.SetBool("Hit2", false);
+            }
+
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Hit3"))
+            {
+                anim.SetBool("Hit3", false);
+            }
         }
-                if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
-        {
-            anim.SetBool("Hit2", false);
-        }
-                if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit3"))
-        {
-            anim.SetBool("Hit3", false);
-        }
-        if(Time.time - lastClickedTime > maxComboDelay)
+
+        if (Time.time - lastClickedTime > maxComboDelay)
         {
             noOfClicks = 0;
         }
-        if(Time.time > nextFireTime)
+
+        if (Time.time > nextFireTime)
         {
-            if(Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 OnClick();
             }
@@ -48,21 +54,28 @@ public class Combat : MonoBehaviour
     {
         lastClickedTime = Time.time;
         noOfClicks++;
-        if(noOfClicks == 1)
+
+        if (noOfClicks == 1)
         {
             anim.SetBool("Hit1", true);
         }
+
         noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
 
-        if(noOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        if (noOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
         {
             anim.SetBool("Hit1", false);
             anim.SetBool("Hit2", true);
+            Debug.Log("Hit1 to Hit2 transition");
         }
-        if(noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
+
+        if (noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
         {
             anim.SetBool("Hit2", false);
             anim.SetBool("Hit3", true);
+            Debug.Log("Hit2 to Hit3 transition");
         }
     }
 }
+
+

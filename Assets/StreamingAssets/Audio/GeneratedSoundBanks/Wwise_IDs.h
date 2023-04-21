6393cdc4698620d93cb481e02528b391532b7564
naybens <83803641+naybens@users.noreply.github.com>
2023-04-20 23:18:09 -0700

    Regenerated soundbank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIENCE_TOWN = 3468413433U;
+        static const AkUniqueID PLAY_AS_MUSIC_FINALBOSS_MAP_1 = 3669722927U;
+        static const AkUniqueID PLAY_AS_MUSIC_LVL_1_MAP_1 = 66508634U;
+        static const AkUniqueID PLAY_AS_MUSIC_LVL_2_MAP_1 = 2743821481U;
+        static const AkUniqueID PLAY_AS_MUSIC_LVL_3_MAP_1 = 485989956U;
+        static const AkUniqueID PLAY_AS_MUSIC_MINIBOSS_MAP_1 = 3753721680U;
+        static const AkUniqueID PLAY_AS_MUSIC_TOWN = 3615401001U;
+        static const AkUniqueID PLAY_AS_MUSIC_TOWN_JESTER = 2756004695U;
+        static const AkUniqueID PLAY_AS_STEP_CONCRETE_1 = 2872411375U;
+        static const AkUniqueID PLAY_AS_STEP_CONCRETE_2 = 2872411372U;
+        static const AkUniqueID PLAY_AS_STEP_HARDWOOD_1 = 1972203458U;
+        static const AkUniqueID PLAY_AS_STEP_HARDWOOD_2 = 1972203457U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FOOTSTEPS_CONCRETE = 82718740U;
+                static const AkUniqueID FOOTSTEPS_GRASS = 7365553U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__

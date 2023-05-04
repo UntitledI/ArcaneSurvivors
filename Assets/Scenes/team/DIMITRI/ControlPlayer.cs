1e5915faa7c7d31607775ab817e66cb195ff2b2a
UntitledI <dimitri98@me.com>
2023-05-04 12:48:49 -0700

    Arrow(axe) implemented

--- Assets/Scenes/team/DIMITRI/ControlPlayer.cs
+++ Assets/Scenes/team/DIMITRI/ControlPlayer.cs
@@ -1,11 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeMove : MonoBehaviour
 {
-
     public float speed = 0.1f;
 
     private Rigidbody player;
+    public GameObject arrowObject;
+    public Transform arrowPoint;
 
     void Start() {
         player = GetComponent<Rigidbody> ();
@@ -19,5 +22,22 @@ public class CubeMove : MonoBehaviour
         Vector3 moveDirection = new Vector3(xDir, 0.0f, zDir);
 
         player.AddForce(moveDirection * speed);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            //ability 1 function
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            //ability 2 function
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            //ability 3 function
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            //ability 4 function
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) {
+            GameObject arrow = Instantiate(arrowObject, arrowPoint.position, transform.rotation);
+            arrow.GetComponent<Rigidbody>().AddForce(transform.forward *25f, ForceMode.Impulse);
+        }                        
     }
-}
+    }
